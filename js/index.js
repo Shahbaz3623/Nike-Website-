c74a20b7016d65f8744d8dfefb2186614b20b7da
Shahbaz3623 <154018886+Shahbaz3623@users.noreply.github.com>
2023-12-17 00:21:13 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const aBtn1 = document.getElementById("a-btn1");
+const aBtn2 = document.getElementById("a-btn2");
+const aBtn3 = document.getElementById("a-btn3");
+const aBtn4 = document.getElementById("a-btn4");
+
+aBtn1.onclick = () => {
+    document.getElementById("a-img").src = "img/red_shoes1.png";
+}
+aBtn2.onclick = () => {
+    document.getElementById("a-img").src = "img/red_shoes2.png";
+}
+aBtn3.onclick = () => {
+    document.getElementById("a-img").src = "img/red_shoes3.png";
+}
+aBtn4.onclick = () => {
+    document.getElementById("a-img").src = "img/red_shoes4.png";
+}
